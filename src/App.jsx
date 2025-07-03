7cc11359dd2c0ded8feb90fd501539a29a3c1f20
rahull0328 <rmmehta2004@gmail.com>
2025-07-03 22:31:01 +0530

    completed register page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Router, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
@@ -9,13 +8,26 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="/" exact element={<Root />} />
           <Route path="/dashboard" exact element={<Home />} />
-          <Route path="/" exact element={<Login />} />
+          <Route path="/login" exact element={<Login />} />
           <Route path="/register" exact element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
+}
+
+const Root = () => {
+  //check if token exists in local storage
+  const isAuthenticated = !!localStorage.getItem("token")
+
+  //redirect to dashboard if authenticated
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/" />
+  )
 }
 
 export default App;
