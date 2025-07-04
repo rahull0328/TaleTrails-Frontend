729877fba2aee551427491682d91748f3c18a735
rahull0328 <rmmehta2004@gmail.com>
2025-07-04 18:09:10 +0530

    started working on navbar and home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import Navbar from "@/components/shared/Navbar";
+import axiosInstance from "@/utils/axiosInstance";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Home
+  const [userInfo, setUserInfo] = useState(null);
+
+  //getting user info
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/user/getUserInfo");
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user);
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        localStorage.clear();
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+
+      {JSON.stringify(userInfo)}
+    </div>
+  );
+};
+
+export default Home;
